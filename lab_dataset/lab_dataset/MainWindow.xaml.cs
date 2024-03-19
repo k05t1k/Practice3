135748285e30c16b4b566db3789208cd28bdaad5
k05t1k <isip_k.m.fionov@mpt.ru>
2024-03-19 10:34:44 +0300

    first_commit

---
+++ lab_dataset/lab_dataset/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using lab_dataset.PizzeriaDataSetTableAdapters;
+
+namespace lab_dataset
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OrdersTableAdapter orders = new OrdersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PizzeriaDataGrid.ItemsSource = orders.GetData();
+        }
+    }
+}
---
+++ lab_ef/lab_ef/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_ef
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    internal class Pizzeria
+    { 
+        public int ClientID;
+        public int PersonalID;
+        public int PizzaID;
+        public int OrdersID;
+
+        public string ClientName {  get; private set; }
+        public string PersonalName { get; private set; }
+        public string PizzaName { get; private set;  }
+        public decimal Price { get; private set; }
+        public Pizzeria(Order orders)
+        {
+            ClientID = orders.Client.ID_Client;
+            ClientName = orders.Client.FirstName;
+
+            PersonalID = orders.Personal.ID_Personal;
+            PersonalName = orders.Personal.FirstName;
+
+            PizzaID = orders.Pizza.ID_Pizza;
+            PizzaName = orders.Pizza.PizzaName;
+
+            OrdersID = orders.ID_Orders;
+            Price = orders.Price;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        private PizzeriaEntities context = new PizzeriaEntities();
+        List<Pizzeria> pizzerias = new List<Pizzeria>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (var item in context.Orders.ToList())
+            {
+                pizzerias.Add(new Pizzeria(item));
+            }
+            PizzeriaDataGrid.ItemsSource = pizzerias;
+
+        }
+    }
+}
